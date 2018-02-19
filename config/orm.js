a8443bc1216b72fa4b720e7816514610e3e09a19
Ben French <ben.jminw@gmail.com>
2018-02-19 15:00:32 -0700

    Inserting and selectAll working.

---
+++ config/orm.js
@@ -0,0 +1,32 @@
+var connection = require('./connection.js');
+
+var orm = {
+    selectAll: function (tableInput, callback) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    insertOne: function (table, columns, values, callback) {
+        var queryString = "INSERT INTO ?? (??) VALUES (?);";
+        connection.query(queryString, [table, columns, values], function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    updateOne: function (table, set, condition, callback) {
+        var queryString =
+            "UPDATE ? set ? where ?";
+
+        connection.query(
+            queryString, [table, set, condition],
+            function (err, result) {
+                if (err) throw err;
+                callback(result);
+            }
+        );
+    }
+};
+
+module.exports = orm;
